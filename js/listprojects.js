7faa031f5f60309def7c019e2aa60eab619ee3bd
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2018-05-26 19:12:19 +0300

    Called the lambdas -- remaining to fill the HTML.

--- js/listprojects.js
+++ js/listprojects.js
@@ -1,101 +1,64 @@
-/**
- * Created by Luca Andrei on 5/25/2018.
- */
-$(document).ready(function(){
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
 
-    // Lift card and show stats on Mouseover
-    $('#product-card').hover(function(){
-        $(this).addClass('animate');
-        $('div.carouselNext, div.carouselPrev').addClass('visible');
-    }, function(){
-        $(this).removeClass('animate');
-        $('div.carouselNext, div.carouselPrev').removeClass('visible');
+
+(function rideScopeWrapper($) {
+    var authToken;
+    WildRydes.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
     });
 
-    // Flip card to the back side
-    $('#view_details').click(function(){
-        $('div.carouselNext, div.carouselPrev').removeClass('visible');
-        $('#product-card').addClass('flip-10');
-        setTimeout(function(){
-            $('#product-card').removeClass('flip-10').addClass('flip90').find('div.shadow').show().fadeTo( 80 , 1, function(){
-                $('#product-front, #product-front div.shadow').hide();
-            });
-        }, 50);
+    $(function onDocReady() {
+        listYoursProjects();
 
-        setTimeout(function(){
-            $('#product-card').removeClass('flip90').addClass('flip190');
-            $('#product-back').show().find('div.shadow').show().fadeTo( 90 , 0);
-            setTimeout(function(){
-                $('#product-card').removeClass('flip190').addClass('flip180').find('div.shadow').hide();
-                setTimeout(function(){
-                    $('#product-card').css('transition', '100ms ease-out');
-                    $('#cx, #cy').addClass('s1');
-                    setTimeout(function(){$('#cx, #cy').addClass('s2');}, 100);
-                    setTimeout(function(){$('#cx, #cy').addClass('s3');}, 200);
-                    $('div.carouselNext, div.carouselPrev').addClass('visible');
-                }, 100);
-            }, 100);
-        }, 150);
-    });
-
-    // Flip card back to the front side
-    $('#flip-back').click(function(){
-
-        $('#product-card').removeClass('flip180').addClass('flip190');
-        setTimeout(function(){
-            $('#product-card').removeClass('flip190').addClass('flip90');
-
-            $('#product-back div.shadow').css('opacity', 0).fadeTo( 100 , 1, function(){
-                $('#product-back, #product-back div.shadow').hide();
-                $('#product-front, #product-front div.shadow').show();
-            });
-        }, 50);
-
-        setTimeout(function(){
-            $('#product-card').removeClass('flip90').addClass('flip-10');
-            $('#product-front div.shadow').show().fadeTo( 100 , 0);
-            setTimeout(function(){
-                $('#product-front div.shadow').hide();
-                $('#product-card').removeClass('flip-10').css('transition', '100ms ease-out');
-                $('#cx, #cy').removeClass('s1 s2 s3');
-            }, 100);
-        }, 150);
-
-    });
-    /* ----  Image Gallery Carousel   ---- */
-
-    var carousel = $('#carousel ul');
-    var carouselSlideWidth = 335;
-    var carouselWidth = 0;
-    var isAnimating = false;
-
-    // building the width of the casousel
-    $('#carousel li').each(function(){
-        carouselWidth += carouselSlideWidth;
-    });
-    $(carousel).css('width', carouselWidth);
-
-    // Load Next Image
-    $('div.carouselNext').on('click', function(){
-        var currentLeft = Math.abs(parseInt($(carousel).css("left")));
-        var newLeft = currentLeft + carouselSlideWidth;
-        if(newLeft == carouselWidth || isAnimating === true){return;}
-        $('#carousel ul').css({'left': "-" + newLeft + "px",
-            "transition": "300ms ease-out"
+        $("#myonoffswitch").click( function(){
+            if($(this).is(':checked') ) {
+                listYoursProjects();
+            } else {
+                listOthersProjects();
+            }
         });
-        isAnimating = true;
-        setTimeout(function(){isAnimating = false;}, 300);
     });
 
-    // Load Previous Image
-    $('div.carouselPrev').on('click', function(){
-        var currentLeft = Math.abs(parseInt($(carousel).css("left")));
-        var newLeft = currentLeft - carouselSlideWidth;
-        if(newLeft < 0  || isAnimating === true){return;}
-        $('#carousel ul').css({'left': "-" + newLeft + "px",
-            "transition": "300ms ease-out"
+    function listYoursProjects() {
+        $.ajax({
+            method: 'GET',
+            url: _config.api.projectInvokeUrl + '/projects',
+            headers: {
+                Authorization: authToken
+            },
+            contentType: 'application/json',
+            success: fillProjects,
+            error: onFailure
         });
-        isAnimating = true;
-        setTimeout(function(){isAnimating = false;}, 300);
-    });
-});
+    }
+
+    function listOthersProjects() {
+        $.ajax({
+            method: 'GET',
+            url: _config.api.projectInvokeUrl + '/projects/others',
+            headers: {
+                Authorization: authToken
+            },
+            contentType: 'application/json',
+            success: fillProjects,
+            error: onFailure
+        });
+    }
+
+    var fillProjects = function (result) {
+        console.log(result);
+    };
+
+
+    var onFailure = function registerSuccess(result) {
+        console.log(result);
+    };
+}(jQuery));
